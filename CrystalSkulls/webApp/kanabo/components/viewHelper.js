9bf26f087825bba1f7689b24a5f04c7413f76241
takayuki-ochiai <go.ahead.oti@gmail.com>
2015-12-13 11:14:12 +0900

    複数の子セグメントを持つ繰り返し項目の追加、保存ができなくなっていた問題を修正

--- CrystalSkulls/webApp/kanabo/components/viewHelper.js
+++ CrystalSkulls/webApp/kanabo/components/viewHelper.js
@@ -8,12 +8,15 @@ module.exports = {
 
 function generateSegment(elements) {
 	var html = "";
+	var repetationElement = null;
 	for (index in elements) {
 		element = elements[index];
 		html += `<div class="element" elementKey="${element.key}">`
 		html += `<span>${element.dispName}</span>`
 		if (element.repetition) {
 			html += `<div class="repetition">`
+			// elementの参照先は書き換えられる場合があるので、退避しておく
+			repetationElement = element;
 		}
 		if (element.childElements == undefined) {
 			html += `<select>`
@@ -28,10 +31,12 @@ function generateSegment(elements) {
 		} else {
 			html += generateSegment(element.childElements);
 		}
-		if (element.repetition) {
-			html += `</div>`
-			html += `<button onclick="addElement('${element.key}')">追加</button>`
-			html += `<button onclick="deleteElement('${element.key}')">削除</button>`
+		if (repetationElement != undefined && repetationElement != null) {
+			html += `</div>`;
+			html += `<button onclick="addElement('${repetationElement.key}')">追加</button>`;
+			html += `<button onclick="deleteElement('${repetationElement.key}')">削除</button>`;
+			// 退避していたrepetationElementを削除
+			repetationElement = null;
 		}
 		html += `</div>`
 	}
